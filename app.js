1c3dde732f1a6736aa53b823f7e1791a4cb8e86f
PhiNSE <93396482+PhiNSE@users.noreply.github.com>
2024-03-13 13:55:12 +0700

    remove view engine

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ const adminRouter = require('./routes/adminRouter');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
